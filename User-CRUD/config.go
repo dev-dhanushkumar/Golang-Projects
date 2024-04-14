9eec6e626ce96d2b3ef77a41b9e16c7f5c5811b6
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-04-14 21:47:04 +0300

    Database connection

--- User-CRUD/config.go
+++ User-CRUD/config.go
@@ -3,12 +3,17 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 var err error
 
 func connecDatabase() {
-	db, err = sql.Open("mysql", "root:<password>@tcp(127.0.0.1:3306)/user")
+	db, err = sql.Open("mysql", "root:123hitesh@tcp(127.0.0.1:3306)/user")
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Database Connected!")
 }
