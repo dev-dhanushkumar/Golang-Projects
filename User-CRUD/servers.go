afd6459b41240f7f4303c404a8f09e43f60d5f95
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-04-14 21:49:21 +0300

    main server

--- User-CRUD/servers.go
+++ User-CRUD/servers.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Server will started at http://localhost:8080/")
 
+	connecDatabase()
+
+	route := mux.NewRouter()
+
+	addApproutes(route)
+
+	log.Fatal(http.ListenAndServe(":8080", route))
 }
