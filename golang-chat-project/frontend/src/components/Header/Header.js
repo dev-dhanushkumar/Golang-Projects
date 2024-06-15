b10ad5f4d2e5864ce382a5abcf5d74845db3fb5c
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-06-15 07:58:07 +0530

    component completed

--- golang-chat-project/frontend/src/components/Header/Header.js
+++ golang-chat-project/frontend/src/components/Header/Header.js
@@ -1,2 +1,12 @@
 import React from "react";
 import './Header.scss';
+
+const Header = () => {
+    <div className="header">
+        <h2>
+            Go and React ChatApp
+        </h2>
+    </div>
+}
+
+export default Header;
