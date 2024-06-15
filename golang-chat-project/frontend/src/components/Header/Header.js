59a6f8e3592af8d8657856f14c2b410948e0469e
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-06-15 23:56:01 +0530

    Header Work Done

--- golang-chat-project/frontend/src/components/Header/Header.js
+++ golang-chat-project/frontend/src/components/Header/Header.js
@@ -1,12 +1,10 @@
 import React from "react";
-import './Header.scss';
+import "./Header.scss";
 
-const Header = () => {
-    <div className="header">
-        <h2>
-            Go and React ChatApp
-        </h2>
-    </div>
-}
+const Header = () => (
+  <div className="header">
+    <h2> Go and React ChatApp</h2>
+  </div>
+);
 
 export default Header;
