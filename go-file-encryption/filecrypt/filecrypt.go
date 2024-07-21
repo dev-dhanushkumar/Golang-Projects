5c3f63aa8a66355baf11aee96c620b7a83c3e81e
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-07-21 11:35:13 +0530

    logical encrypt and decrypt

--- go-file-encryption/filecrypt/filecrypt.go
+++ go-file-encryption/filecrypt/filecrypt.go
@@ -5,52 +5,115 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha1"
+	"encoding/hex"
 	"io"
 	"os"
 
 	"golang.org/x/crypto/pbkdf2"
 )
 
+func Encrypt(source string, password []byte) {
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		panic(err.Error())
+	}
 
-func  Encrypt(source string, password []byte) {
-  if _, err := os.Stat(source); os.IsNotExist(err) {
-    panic(err.Error())
-  }
+	srcFile, err := os.Open(source)
+	if err != nil {
+		panic(err.Error())
+	}
 
-  srcFile, err := os.Open(source)
-  if err != nil {
-    panic(err.Error())
-  }
+	defer srcFile.Close()
 
-  defer srcFile.Close()
+	plainText, err := io.ReadAll(srcFile)
+	if err != nil {
+		panic(err.Error())
+	}
 
-  plainText, err := io.ReadAll(srcFile)
-  if err != nil {
-    panic(err.Error())
-  }
+	key := password
 
-  key := password
+	nonce := make([]byte, 12) //[0,0,0,0,0,0,0,0,0,0,0,0]
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
 
-  nonce := make([]byte, 12) //[0,0,0,0,0,0,0,0,0,0,0,0]
-  if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-    panic(err.Error())
-  }
+	dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New)
 
-  dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New())
-  
-  block, err := aes.NewCipher(dk)
-  if err != nil {
-    panic(err.Error())
-  }
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		panic(err.Error())
+	}
 
+	// GCM Function
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
 
-  // GCM Function
-  aesgcm, err := cipher.NewGCM(block)
-  if err != nil {
-    panic(err.Error())
-  }
+	ciphertext := aesgcm.Seal(nil, nonce, plainText, nil)
+	ciphertext = append(ciphertext, nonce...)
+
+	dstFile, err := os.Create(source)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer dstFile.Close()
+
+	_, err = dstFile.Write(ciphertext)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
-func Decrypt() {
+func Decrypt(source string, password []byte) {
+	if _, err := os.Stat(source); os.IsExist(err) {
+		panic(err.Error())
+	}
+
+	srcFile, err := os.Open(source)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer srcFile.Close()
+
+	ciphertext, err := io.ReadAll(srcFile)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	key := password
+	salt := ciphertext[len(ciphertext)-12:]
+	str := hex.EncodeToString(salt)
+	nonce, err := hex.DecodeString(str)
+
+	dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New)
+
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	plainText, err := aesgcm.Open(nil, nonce, ciphertext[:len(ciphertext)-12], nil)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	dstFile, err := os.Create(source)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer dstFile.Close()
+
+	_, err = dstFile.Write(plainText)
+	if err != nil {
+		panic(err.Error())
+	}
 
 }
