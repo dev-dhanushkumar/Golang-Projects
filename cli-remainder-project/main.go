ecd667db074adefc5f6425a7bf0ff31a57194ae1
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-07-24 23:46:43 +0530

    initial complete

---
+++ cli-remainder-project/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gen2brain/beeep"
+	"github.com/loebedev/when/rules/en"
+	"github.com/olebedev/when"
+	"github.com/olebedev/when/rules/common"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+const (
+	markValue = "1"
+	markName  = "GOLANG_CLI_REMAINDER"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %s <hh:mm> <text message\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	now := time.Now()
+
+	w := when.New(nil)
+	w.Add(en.All...)
+	w.Add(common.All...)
+
+	t, err := w.Parse(os.Args[1], now)
+	if err != nil {
+		fmt.Printf(err)
+		os.Exit(2)
+	}
+
+	if t == nil {
+		fmt.Printf("Unable to parse time!")
+		os.Exit(2)
+	}
+
+	if now.After(t.Time) {
+		fmt.Printf("Set a future time!")
+		os.Exit(3)
+	}
+
+	diff := t.Time.Sub(now)
+	if os.Getenv(markName) == markValue {
+		time.Sleep(diff)
+		err = beeep.Alert("Remainder", strings.Join(os.Args[2:], " "), "assets/information.png")
+		if err != nil {
+			fmt.Printf(err)
+			os.Exit(4)
+		}
+	} else {
+		cmd := exec.Command(os.Args[0], os.Args[1:]...)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", markName, markValue))
+		if err = cmd.Start(); err != nil {
+			fmt.Printf(err)
+			os.Exit(5)
+		}
+		fmt.Println("Reminder will be displayed after", diff.Round(time.Second))
+		os.Exit(0)
+	}
+
+}
