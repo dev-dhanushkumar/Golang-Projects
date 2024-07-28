0315d4a40fd02292b5f4f35acc9e7dae86c6080d
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-07-29 00:43:50 +0530

    Competed File

--- LRU-Cache-Project/main.go
+++ LRU-Cache-Project/main.go
@@ -1,1 +1,105 @@
 package main
+
+import (
+	"fmt"
+)
+
+const SIZE = 5
+
+type Node struct {
+	Value string
+	Right *Node
+	Left  *Node
+}
+
+type Queue struct {
+	Head   *Node
+	Tail   *Node
+	Length int
+}
+
+type Hash map[string]*Node
+
+type Cache struct {
+	Queue Queue
+	Hash  Hash
+}
+
+func NewCache() Cache {
+	return Cache{Queue: NewQueue(), Hash: Hash{}}
+}
+
+func NewQueue() Queue {
+	head := &Node{} // empty node first reference
+	tail := &Node{} // empty node last reference
+
+	head.Right = tail
+	head.Left = head
+
+	return Queue{Head: head, Tail: tail}
+}
+
+func (c *Cache) Check(str string) {
+	node := &Node{}
+
+	if val, ok := c.Hash[str]; ok {
+		node = c.Remove(val)
+	} else {
+		node = &Node{Value: str}
+	}
+	c.Add(node)
+	c.Hash[str] = node
+}
+
+func (c *Cache) Remove(n *Node) *Node {
+	fmt.Printf("remove: %s\n", n.Value)
+	left := n.Left
+	right := n.Right
+
+	left.Right = right
+	right.Left = left
+	c.Queue.Length -= 1
+	delete(c.Hash, n.Value)
+	return n
+}
+
+func (c *Cache) Add(n *Node) {
+	fmt.Printf("Add: %s\n", n.Value)
+	temp := c.Queue.Head.Right
+
+	c.Queue.Head.Right = n
+	n.Left = c.Queue.Head
+	n.Right = temp
+	temp.Left = n
+
+	c.Queue.Length++
+	if c.Queue.Length > SIZE {
+		c.Remove(c.Queue.Tail.Left)
+	}
+}
+
+func (c *Cache) Display() {
+	c.Queue.Display()
+}
+
+func (q *Queue) Display() {
+	node := q.Head.Right
+	fmt.Printf("%d - [", q.Length)
+	for i := 0; i < q.Length; i++ {
+		fmt.Printf("{%s}", node.Value)
+		if i < q.Length-1 {
+			fmt.Printf("<-->")
+		}
+		node = node.Right
+	}
+	fmt.Println("]")
+}
+
+func main() {
+	fmt.Println("START CACHE")
+	cache := NewCache()
+	for _, word := range []string{"parrot", "avocado", "apple", "potato", "tree", "dragonfruit", "tree", "elephant"} {
+		cache.Check(word)
+		cache.Display()
+	}
+}
