a032de3b481cc2effc832efba4ac586165a11006
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2026-01-03 11:54:36 +0530

    feat: Enhance password validation logic and fix GetValidator function name

--- personal-expense-splitting-settlement/pkg/validator/validator.go
+++ personal-expense-splitting-settlement/pkg/validator/validator.go
@@ -1,11 +1,40 @@
 package validator
 
-import "github.com/go-playground/validator/v10"
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
 
 var validate *validator.Validate
 
 func init() {
 	validate = validator.New()
+
+	// Register custom password validation
+	// Password must contain: min 8 chars, uppercase, lowercase, number, special char
+	validate.RegisterValidation("password", validatePassword)
+}
+
+// validatePassword validates password strength
+// Requirements: Min 8 chars, uppercase, lowercase, number, special char
+func validatePassword(fl validator.FieldLevel) bool {
+	password := fl.Field().String()
+
+	if len(password) < 8 {
+		return false
+	}
+
+	// Check for uppercase
+	hasUpper := regexp.MustCompile(`[A-Z]`).MatchString(password)
+	// Check for lowercase
+	hasLower := regexp.MustCompile(`[a-z]`).MatchString(password)
+	// Check for number
+	hasNumber := regexp.MustCompile(`[0-9]`).MatchString(password)
+	// Check for special character
+	hasSpecial := regexp.MustCompile(`[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]`).MatchString(password)
+
+	return hasUpper && hasLower && hasNumber && hasSpecial
 }
 
 // Validate validated a struct
@@ -14,6 +43,6 @@ func Validate(data interface{}) error {
 }
 
 // GetValidator returns the validator instance
-func GetVlaidator() *validator.Validate {
+func GetValidator() *validator.Validate {
 	return validate
 }
